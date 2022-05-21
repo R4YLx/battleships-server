44cf96bbe10c4bf4b28caab539ea61de2aa6ee20
Raymond Lam <raymond.m.lam@gmail.com>
2022-05-21 11:00:13 +0200

    emitting hit to room

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -87,7 +87,7 @@ const handleMiss = function (target, username) {
 	let miss = target.replace("e", "m");
 	console.log(`Enemy clicked on ${target} and on your board it is ${miss}`);
 
-	// io.to("game").emit("player:missed", miss);
+	io.to("game").emit("player:missed", miss);
 };
 
 /**
